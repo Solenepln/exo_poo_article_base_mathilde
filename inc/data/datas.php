736d4779ca4c803b6374b191da37d2013f213960
Solene <solene.paulin24@gmail.com>
2022-02-01 11:16:15 +0100

    transform_table_with_objects

--- inc/data/datas.php
+++ inc/data/datas.php
@@ -2,42 +2,19 @@
 
 // Les différents articles sous forme de tableaux
 $dataArticlesList = [
-    1 => [
-        "title" => "Comment caresser son chien dans le sens du poil",
-        "content"=> "<p> Il est important de caresser son chien dans le sens du poil pour notamment éviter les poils incarnés et la mauvaise humeur </p>",
-        "author" =>"Mathilde",
-        "date"=>'2020-07-13',
-        "category" =>'Bien-être'
-    ],
-    2 => [
-        "title" => "Pourquoi les chiens sentent-ils mauvaise de la bouche ?",
-        "content"=> "<p> Senteur poubelle ou senteur caniveau, l'haleine de nos canidés est un vrai problème du quotidien. Il existe pourtant des solutions simples </p>",
-        "author" =>"Nicolas",
-        "date"=>'2020-03-25',
-        "category" =>'Soins et santé'
-    ],
-    3 => [
-        "title" => "Top 10 des meilleurs jouets à tester",
-        "content"=> "<p> Peluche, baballe, pouet pouet, jeux d'intelligence et d'agilité, nous avons sélectionné pour vous les meilleurs jeux 2020 pour votre animal </p>",
-        "author" =>"Pierre",
-        "date"=>'2020-12-31',
-        "category" =>'Loisirs'
-    ],
-    4 => [
-        "title" => "Mon chien mord, que faire ?",
-        "content"=> "<p> Cette sale bête s'attaque à tout le monde, j'ai déjà 5 procès sur le dos, je ne sais plus quoi faire, il me fait peur </p>",
-        "author" =>"Colette",
-        "date"=>'2020-01-05',
-        "category" =>'Education'
-    ],
-
-    5 => [
-        "title" => "Super génial ",
-        "content"=> "<p> Truc Truc Truc  </p>",
-        "author" =>"Colette",
-        "date"=>'2020-01-05',
-        "category" =>'Education'
-    ],
+    1 => new Article("Comment caresser son chien dans le sens du poil",
+    "<p> Il est important de caresser son chien dans le sens du poil pour notamment éviter 
+    les poils incarnés et la mauvaise humeur </p>",
+    "Mathilde", '2023-04-25', 'Bien-être'), 
+    
+    2 => new Article("Pourquoi les chiens sentent-ils mauvaise de la bouche ?",
+    "<p> Senteur poubelle ou senteur caniveau, l'haleine de nos canidés est un vrai problème du quotidien. Il existe pourtant des solutions simples </p>",
+    "Nicolas",'2020-03-25','Soins et santé'),
+    
+    
+    3 => new Article("Top 10 des meilleurs jouets à tester",
+    "<p> Peluche, baballe, pouet pouet, jeux d'intelligence et d'agilité, nous avons sélectionné pour vous les meilleurs jeux 2020 pour votre animal </p>",
+    "Pierre",'2020-12-31','Loisirs')
 ];
 
 // Les catégories
